9d0d59dafa8b39653e8f149ecee0eead6f36c24d
Takshak Mudgal <takshakmudgal@gmail.com>
2025-12-22 02:48:33 +0530

    sending & receiving client response

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::{
-    io::Write,
-    net::{Ipv4Addr, TcpListener},
+    io::{BufRead, BufReader, BufWriter, Write},
+    net::{Ipv4Addr, TcpListener, TcpStream},
 };
 
 static LOCALHOST: Ipv4Addr = Ipv4Addr::LOCALHOST;
+
+fn handle_client(stream: TcpStream) {
+    println!("Client connected");
+
+    let mut writer = BufWriter::new(&stream);
+    writer
+        .write_all(format!("220 {LOCALHOST} ESMTP Ready\r\n").as_bytes())
+        .expect("could not write");
+    writer.flush().expect("could not flush");
+
+    let mut reader = BufReader::new(&stream);
+    let mut response = String::new();
+    reader.read_line(&mut response).expect("could not read");
+    println!("Server received {}", response);
+}
 
 fn main() {
     let port = 3000;
     let listener = TcpListener::bind(format!("{LOCALHOST}:{port}")).unwrap();
     for stream in listener.incoming() {
-        let message = format!("220 {LOCALHOST} ESMTP Ready\r\n");
-        let _ = stream.unwrap().write(message.as_bytes());
+        let stream = stream.expect("Unable to accept");
+        handle_client(stream);
     }
 }
